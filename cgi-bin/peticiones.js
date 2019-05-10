cd6ad10e07e1c7863c583e77894832ac195d3d35
Horus S. Rico <horus.sandoval@carpathialab.com>
2019-05-10 14:10:41 -0500

    Se agrega el módulo de fibonacci

--- cgi-bin/peticiones.js
+++ cgi-bin/peticiones.js
@@ -1,5 +1,7 @@
 const svr = require('http');
 const usr = require('url');
+const fibo = require('./fibonacci.js');
+
 //Aquí hay que jalar los 'require' adicionales que puedan hacer falta como FileSystem, etc.
 const host = '127.0.0.1';
 const puerto = '8080';
@@ -8,7 +10,9 @@ const servidor = svr.createServer((pet, resp) => {
 	let respuesta = '';
 	resp.setHeader('Content-Type', 'text/plain');
 	if (pet.method == 'GET') {
-		respuesta = procesaGet(pet);
+		//respuesta = procesaGet(pet);
+		console.log("Una petición");
+		console.log(fibo.doFibonacci(10));
 		resp.statusCode = 200;
 	} else if (pet.method == 'POST') {
 		respuesta = procesaPost(pet);
@@ -29,5 +33,6 @@ function procesaGet(peticion) {
 
 function procesaPost(peticion) {
 	//Igualmente, aquí hay que obtener el valor que venga en la URL...
-	
+	console.log(peticion);
+	console.log(peticion.body);
 }
